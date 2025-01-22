4716f85ebe7b8bb9d2149235b11c8d003d7202b1
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-22 17:03:30 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using EventEaseApp;
+using EventEaseApp.Data;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<EventService>();
+
+await builder.Build().RunAsync();
 
