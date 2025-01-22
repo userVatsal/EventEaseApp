a0e0255dfff9a088c9795ae4a81bf39f8e10c3e5
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-22 17:02:36 +0000

    Update EventService.cs

--- Data/EventService.cs
+++ Data/EventService.cs
@@ -1,1 +1,27 @@
+using System.Collections.Generic;
+
+namespace EventEaseApp.Data
+{
+    public class EventService
+    {
+        private List<Event> events = new List<Event>();
+
+        public void AddEvent(Event event)
+        {
+            events.Add(event);
+        }
+
+        public List<Event> GetEvents()
+        {
+            return events;
+        }
+    }
+
+    public class Event
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+    }
+}
 
